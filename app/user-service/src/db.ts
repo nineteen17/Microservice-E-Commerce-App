57c9fe2929309ef3f62442227564bc38e4e0a284
nineteen17 <nineteen17@Laptop-ZZ.localdomain>
2024-02-03 00:53:55 +1300

    setup first service

---
+++ app/user-service/src/db.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.USER_MONGO_URI as string);
+    console.log('MongoDB connection successful.');
+  } catch (err) {
+    console.error("MongoDB connection failed.");
+    process.exit(1);
+  }
+};
+
+
+export default connectDB;
