1ff6109c8ce83169f65511dc247898065ebe0c3a
nineteen17 <nineteen17@Laptop-ZZ.localdomain>
2024-02-08 21:39:11 +1300

    updated ingress using helm to to fix path routing

--- app/user-service/src/app.ts
+++ app/user-service/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
+
 import dotenv from 'dotenv'
 
 
@@ -10,21 +10,6 @@ const app = express()
 app.use(cookieParser());
 // Middleware
 
-const allowedOrigins = ['http://localhost:3000'];
-
-app.use(
-  cors({
-    origin: function(origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-  })
-)
 
 app.use(express.json())
 
