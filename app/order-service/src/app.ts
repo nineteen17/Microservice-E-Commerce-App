0a516c3524108b68409610405ae28fa2f514d9a4
nineteen17 <nineteen17@Laptop-ZZ.localdomain>
2024-02-21 23:57:38 +1300

    created the order-service db-model, server, k8s pod

--- app/order-service/src/app.ts
+++ app/order-service/src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
-import userRouter from './routes/userRoutes';
-import authRouter from './routes/authRoutes';
+
 
 const app = express();
 
@@ -10,7 +9,6 @@ app.use(cookieParser());
 app.use(express.json())
 
 // Routes
-app.use('/user-service/',userRouter)
-app.use('/user-service/', authRouter)
+
 
 export default app
